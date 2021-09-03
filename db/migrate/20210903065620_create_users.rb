c1a3170d1fb4f0e437bf0d934e97a97d62d2c12e
Musa Abdillah <musa.abdillah@ottodigital.id>
2021-09-03 14:06:51 +0700

    description user as text

--- db/migrate/20210903065620_create_users.rb
+++ db/migrate/20210903065620_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :avatar
       t.string :name
-      t.string :description
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_09_03_065821) do
   create_table "users", force: :cascade do |t|
     t.string "avatar"
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
